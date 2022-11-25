131e3b26fc8718f1960631e11cc1fccfea11e221
Gabriel Lohest <16205@ecam.be>
2022-11-25 13:12:40 +0100

    Try some stuff with mnist

---
+++ backend/mnist training.py
@@ -0,0 +1,96 @@
+import cv2
+import numpy as np
+from sklearn.preprocessing import LabelBinarizer
+from tensorflow.keras.datasets import mnist
+
+
+def load_mnist_dataset():
+
+  # load data from tensorflow framework
+  ((trainData, trainLabels), (testData, testLabels)) = mnist.load_data() 
+
+  # Stacking train data and test data to form single array named data
+  data = np.vstack([trainData, testData]) 
+
+  # Vertical stacking labels of train and test set
+  labels = np.hstack([trainLabels, testLabels]) 
+
+  # return a 2-tuple of the MNIST data and labels
+  return (data, labels)
+
+def load_az_dataset(datasetPath):
+
+  # List for storing data
+  data = []
+  
+  # List for storing labels
+  labels = []
+  
+  for row in open(datasetPath): #Openfile and start reading each row
+    #Split the row at every comma
+    row = row.split(",")
+    
+    #row[0] contains label
+    label = int(row[0])
+    
+    #Other all collumns contains pixel values make a saperate array for that
+    image = np.array([int(x) for x in row[1:]], dtype="uint8")
+    
+    #Reshaping image to 28 x 28 pixels
+    image = image.reshape((28, 28))
+    
+    #append image to data
+    data.append(image)
+    
+    #append label to labels
+    labels.append(label)
+    
+  #Converting data to numpy array of type float32
+  data = np.array(data, dtype='float32')
+  
+  #Converting labels to type int
+  labels = np.array(labels, dtype="int")
+  
+  return (data, labels)
+
+
+(digitsData, digitsLabels) = load_mnist_dataset()
+
+(azData, azLabels) = load_az_dataset('backend/training/A_Z Handwritten Data.csv')
+
+
+# the MNIST dataset occupies the labels 0-9, so let's add 10 to every A-Z label to ensure the A-Z characters are not incorrectly labeled 
+
+azLabels += 10
+
+# stack the A-Z data and labels with the MNIST digits data and labels
+
+data = np.vstack([azData, digitsData])
+labels = np.hstack([azLabels, digitsLabels])
+
+# Each image in the A-Z and MNIST digts datasets are 28x28 pixels;
+# However, the architecture we're using is designed for 32x32 images,
+# So we need to resize them to 32x32
+
+data = [cv2.resize(image, (32, 32)) for image in data]
+data = np.array(data, dtype="float32")
+
+# add a channel dimension to every image in the dataset and scale the
+# pixel intensities of the images from [0, 255] down to [0, 1]
+
+data = np.expand_dims(data, axis=-1)
+data /= 255.0
+
+
+le = LabelBinarizer()
+labels = le.fit_transform(labels)
+
+counts = labels.sum(axis=0)
+
+# account for skew in the labeled data
+classTotals = labels.sum(axis=0)
+classWeight = {}
+
+# loop over all classes and calculate the class weight
+for i in range(0, len(classTotals)):
+  classWeight[i] = classTotals.max() / classTotals[i]
