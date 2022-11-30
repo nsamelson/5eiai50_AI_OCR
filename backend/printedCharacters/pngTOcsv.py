bc401c830bd01fa62dd775d9c57cc4b76960ae14
nsamelson <salocin.sam@gmail.com>
2022-11-30 12:37:31 +0100

    printedChars model

--- backend/printedCharacters/pngTOcsv.py
+++ backend/printedCharacters/pngTOcsv.py
@@ -1,25 +1,52 @@
+import os
 from PIL import Image
 import numpy as np
 
 
 
-# 1. Read image
-img = Image.open("backend/training/printed/image_0_12456.png")
- 
-# 2. Convert image to NumPy array
-arr = np.asarray(img)
-print(arr.shape)
-# (771, 771, 3)
-# 3. Convert 3D array to 2D list of lists
-lst = []
-for row in arr:
-    tmp = []
-    for col in row:
-        tmp.append(str(col))
-    lst.append(tmp)
+labels = ['0','1','2','3','4','5','6','7','8','9',
+    'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+    'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
-    
+
+
+directory = "backend/training/printed/"
+csv = []
+
+# Create headers of the CSV
+headers = []
+for i in range(785):
+    if(i ==0):
+        headers.append("label")
+    else:
+        headers.append(f"pixel{i}")
+csv.append(headers)
+
+# add each image as a row
+for image in os.listdir(f"backend/training/printed/"):
+    f = os.path.join(directory, image)
+
+    # 1. Read image
+    img = Image.open(f)
+
+    # 2. Convert image to NumPy array
+    arr = np.asarray(img)
+
+    # 3. find the label of the character
+    name = image.split("_")
+    label = labels.index(name[1])
+
+    lst = []
+    lst.append(str(label))
+    for row in arr:
+        for col in row:
+            Grayscale = 0.299 * col[0] + 0.587 * col[1] + 0.114 * col[2]
+            lst.append(str(Grayscale))
+ 
+    csv.append(lst)
+
+
 # 4. Save list of lists to CSV
-with open('my_file.csv', 'w') as f:
-    for row in lst:
+with open('backend/training/digitsAndChars.csv', 'w') as f:
+    for row in csv:
         f.write(','.join(row) + '\n')
