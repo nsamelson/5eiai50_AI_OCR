8e412a57eacd43ae84307a4fe22cc6dd75206bf0
nsamelson <salocin.sam@gmail.com>
2022-12-07 15:12:30 +0100

    can upload an image on the server

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,9 +7,19 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { initializeApp  } from 'firebase/app';
+import { environment } from '../environments/environment';
+
+initializeApp(environment.firebaseConfig);
+
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule, 
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAQN5poqH_lTkD8GdxfqQ7WI-IqkW3CxQc",    
+    authDomain: "ocr-des-enfers.firebaseapp.com",    
+    projectId: "ocr-des-enfers",    
+    storageBucket: "ocr-des-enfers.appspot.com",    
+    messagingSenderId: "896152135137",    
+    appId: "1:896152135137:web:7b2562a960c5c034dbdea0"    
+  }
 };
