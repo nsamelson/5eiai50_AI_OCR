3e763f5d4c5db2b1904ad5b21ae82c5d924ebdb7
nsamelson <salocin.sam@gmail.com>
2022-12-07 17:09:14 +0100

    added popup to show image in bigger size

--- frontend/src/app/imported/imported.page.ts
+++ frontend/src/app/imported/imported.page.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { getStorage, ref, listAll, FirebaseStorage, ListOptions, getDownloadURL, deleteObject } from "firebase/storage";
+import { OverlayEventDetail } from '@ionic/core/components';
+import { IonModal } from '@ionic/angular';
 
 @Component({
   selector: 'app-imported',
@@ -10,6 +12,9 @@ export class ImportedPage implements OnInit {
   storage: FirebaseStorage
   urlList: string[] = [];
   imgRefList: string[] = [];
+  urlOfImg: string | undefined;
+
+  @ViewChild(IonModal) modal: IonModal | undefined;
 
   constructor() {
     this.storage = getStorage();
@@ -49,6 +54,25 @@ export class ImportedPage implements OnInit {
     });
   }
 
+  openModal(index: any){
+    this.urlOfImg = this.urlList[index];
+    this.modal?.present();
+  }
+
+  cancelModal() {
+    this.modal!.dismiss(null, 'cancel');
+  }
+
+  confirmModal() {
+    this.modal!.dismiss("this.name", 'confirm');
+  }
   
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm') {
+      // this.message = `Hello, ${ev.detail.data}!`;
+      console.log(ev.detail.data)
+    }
+  }
 
 }
