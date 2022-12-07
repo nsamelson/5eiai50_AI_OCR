8ed43b03a6a8dcd78df3edecdea25e7c6397153e
nsamelson <salocin.sam@gmail.com>
2022-12-07 16:48:13 +0100

    can see the unprocessed images and delete them

--- frontend/src/app/imported/imported.page.ts
+++ frontend/src/app/imported/imported.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { getStorage, ref, listAll, FirebaseStorage, ListOptions, getDownloadURL, deleteObject } from "firebase/storage";
 
 @Component({
   selector: 'app-imported',
@@ -6,10 +7,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./imported.page.scss'],
 })
 export class ImportedPage implements OnInit {
+  storage: FirebaseStorage
+  urlList: string[] = [];
+  imgRefList: string[] = [];
 
-  constructor() { }
+  constructor() {
+    this.storage = getStorage();
 
+   }
+  
   ngOnInit() {
+    // Create a reference under which you want to list
+    const listRef = ref(this.storage, 'unprocessed');
+    this.urlList = [];
+    this.imgRefList = [];
+
+    // Find all the prefixes and items.
+    listAll(listRef)
+      .then((res) => {
+        res.items.forEach((itemRef) => {
+          
+
+          getDownloadURL(itemRef).then((downloadURL) => {
+            this.urlList.push(downloadURL);
+            this.imgRefList.push(itemRef.name);
+          });
+          
+        });
+        console.log(this.urlList)
+      }).catch((error) => {
+        // Uh-oh, an error occurred!
+      });
   }
+
+  async deleteImage(index: any){
+    const storageRef = ref(this.storage, "unprocessed/"+this.imgRefList[index]);
+    deleteObject(storageRef).then(() => {
+      this.ngOnInit()
+    }).catch((error) => {
+      // Uh-oh, an error occurred!
+    });
+  }
+
+  
 
 }
