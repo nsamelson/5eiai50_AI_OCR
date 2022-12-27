f318396b0fc263a29f040389d88b8ddb6224f20b
nsamelson <salocin.sam@gmail.com>
2022-12-27 11:26:41 +0100

    connected backend to firebase

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,18 @@
 from flask import *
 from flask_cors import CORS
-# import json
+import firebase_admin
+from firebase_admin import storage
+from firebase_admin import credentials
+
+# Firebase setup
+cred = credentials.Certificate('backend/config/firebase.json')
+firebase_admin.initialize_app(cred, {
+   'storageBucket': 'ocr-des-enfers.appspot.com'
+})
+
+bucket = storage.bucket()
+
+# Flask setup
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
@@ -8,26 +20,35 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 # TEST SERVER ONLY
 @app.route('/')
 def hello_world():
-    response = jsonify({'some': 'data'})
-    return response
+   response = jsonify({'some': 'data'})
+   return response
 
 # PROCESS IMAGE INTO OCR
 @app.route('/process/', methods=['POST'])
 def processData():
-    items = request.get_json()
-    imageName = items["img"]
-    imageUrl = items["url"]
+   items = request.get_json()
+   imageName = items["img"]
+   imageUrl = items["url"]
 
 
-    print(imageName, imageUrl)
-    # TODO: call the preprocessing program
-    # TODO: call the processing program (which will loop through every bouding box and send it to model)
-    # TODO: call the post processing program which will send back a json
-    # TODO: send the json to firebase/storage/processed in a folder with its corresponding image
-    # TODO: send back a success response!
-    
-    response = jsonify({'success': 'The image has been processed!!!'})
-    return response
+   print(imageName, imageUrl)
+
+   blob = bucket.blob("unprocessed/"+imageName)
+   blob.download_to_filename("backend/temp/"+imageName) 
+
+   # TODO: call the preprocessing program 
+   #    - detect text zone
+   #    - (detect words)
+   #    - detect characters and make bounding boxes in 28*28px
+   # TODO: iterative call the processing program (which will loop through every bouding box and send it to model)
+   # TODO: call the post processing program which will send back a json
+   # TODO: send the json to firebase/storage/processed in a folder with its corresponding image
+   # TODO: send back a success response!
+
+   # TODO: remove temp content
+   
+   response = jsonify({'success': 'The image has been processed!!!'})
+   return response
 
 # GET LIST OF UNPROCESSED IMAGES
 
