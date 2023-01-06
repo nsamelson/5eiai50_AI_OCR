99b82523972fb940b1954feb3b364a137f9aab4e
nsamelson <salocin.sam@gmail.com>
2023-01-06 21:44:33 +0100

    list all processed images and pdfs

--- frontend/src/app/processed/processed.page.ts
+++ frontend/src/app/processed/processed.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core/components';
 import { deleteObject, FirebaseStorage, getDownloadURL, getStorage, listAll, ref } from 'firebase/storage';
 
 @Component({
@@ -10,6 +12,11 @@ export class ProcessedPage implements OnInit {
   storage: FirebaseStorage
   urlList: string[] = [];
   imgRefList: string[] = [];
+  urlOfImg: string ="";
+  imageName: string | undefined;
+  urlDict: {[key: number]: string[]} = {}
+
+  @ViewChild(IonModal) modal: IonModal | undefined;
 
   constructor() {
     this.storage = getStorage();
@@ -28,16 +35,25 @@ export class ProcessedPage implements OnInit {
           // Find all the items inside the folders
           listAll(folderRef)
             .then((res) => {
-              res.items.forEach((itemRef) => {                
-
+              res.items.forEach((itemRef, index) => {                
+                var jsonUrl =""
+                var imageUrl =""
                 getDownloadURL(itemRef).then((downloadURL) => {
 
-                  // TODO: ADD IF comparison to differentiate between image and json document
-                  this.urlList.push(downloadURL);
-                  this.imgRefList.push(itemRef.name);
+                  
+                  if (downloadURL.includes(".json")){
+                    jsonUrl = downloadURL
+                  }
+                  else{
+                    imageUrl = downloadURL
+                    this.urlList.push(downloadURL);
+                    this.imgRefList.push(itemRef.name);
+                  }
+                  
 
                   console.log(itemRef);
                 });
+                this.urlDict[index] = [imageUrl,jsonUrl]
                 
               });
               
@@ -51,6 +67,50 @@ export class ProcessedPage implements OnInit {
       }).catch((error) => {
         // Uh-oh, an error occurred!
       });
+  }
+
+  getFileExtension(url: string) {
+    const urlObject = new URL(url);
+    const fileName = urlObject.pathname.split('/').pop();
+    return fileName!.split('.').pop();
+  }
+
+  async deleteImage(index: any){
+    // const storageRef = ref(this.storage, "processed/"+this.imgRefList[index]);
+    // deleteObject(storageRef).then(() => {
+    //   this.ngOnInit()
+    // }).catch((error) => {
+    //   // Uh-oh, an error occurred!
+    // });
+  }
+
+
+  openModal(index: any){
+    this.urlOfImg = this.urlList[index];
+
+    // const fileExtension = this.getFileExtension(this.urlOfImg)
+    // if (fileExtension == "pdf"){
+      
+    // }
+
+    this.imageName = this.imgRefList[index];
+    // this.imageRef = this.imgRefList[index];
+    this.modal?.present();
+  }
+
+  cancelModal() {
+    this.modal!.dismiss(null, 'cancel');
+  }
+
+  confirmModal() {
+    this.modal!.dismiss("this.name", 'confirm');
+  }
+  
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm') {
+      // this.processData()
+    }
   }
 
   // async deleteImage(index: any){
