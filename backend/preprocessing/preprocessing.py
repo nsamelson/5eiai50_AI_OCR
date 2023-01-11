a94b2c8ee769db74f140ebb622bc169bf594050c
Gabriel Lohest <16205@ecam.be>
2023-01-11 15:57:19 +0100

    Remove text detector argparser and switch to function call

---
+++ backend/preprocessing/preprocessing.py
@@ -0,0 +1,4 @@
+import textbounding.opencv_text_detection_image as textDetector
+
+textDetector.cropWords("./backend/preprocessing/textbounding/images/Loremipsum1.png")
+
--- backend/preprocessing/textbounding/opencv_text_detection_image.py
+++ backend/preprocessing/textbounding/opencv_text_detection_image.py
@@ -7,169 +7,159 @@ import cv2
 import shutil
 import os
 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", type=str,
-                help="path to input image")
-ap.add_argument("-east", "--east", type=str,
-                help="path to input EAST text detector", default="./backend/preprocessing/textbounding/frozen_east_text_detection.pb")
-ap.add_argument("-c", "--min-confidence", type=float, default=0.5,
-                help="minimum probability required to inspect a region")
-ap.add_argument("-w", "--width", type=int, default=1280,
-                help="resized image width (should be multiple of 32)")
-ap.add_argument("-e", "--height", type=int, default=1280,
-                help="resized image height (should be multiple of 32)")
-args = vars(ap.parse_args())
+def cropWords(imagePath, minConfidence=0.5, width=1280, height=1280):
 
-# load the input image and grab the image dimensions
-image = cv2.imread(args["image"])
-orig = image.copy()
-(H, W) = image.shape[:2]
+    # load the input image and grab the image dimensions
+    image = cv2.imread(imagePath)
+    orig = image.copy()
+    (H, W) = image.shape[:2]
 
-# set the new width and height and then determine the ratio in change for both the width and height
-(newW, newH) = (args["width"], args["height"])
-rW = W / float(newW)
-rH = H / float(newH)
+    # set the new width and height and then determine the ratio in change for both the width and height
+    (newW, newH) = (width, height)
+    rW = W / float(newW)
+    rH = H / float(newH)
 
-# resize the image and grab the new image dimensions
-image = cv2.resize(image, (newW, newH))
-(H, W) = image.shape[:2]
+    # resize the image and grab the new image dimensions
+    image = cv2.resize(image, (newW, newH))
+    (H, W) = image.shape[:2]
 
-# define the two output layer names for the EAST detector model that we are interested -- the first is the output probabilities and the 
-# second can be used to derive the bounding box coordinates of text
-layerNames = [
-    "feature_fusion/Conv_7/Sigmoid",
-    "feature_fusion/concat_3"]
+    # define the two output layer names for the EAST detector model that we are interested -- the first is the output probabilities and the 
+    # second can be used to derive the bounding box coordinates of text
+    layerNames = [
+        "feature_fusion/Conv_7/Sigmoid",
+        "feature_fusion/concat_3"]
 
-# load the pre-trained EAST text detector
-print("[INFO] loading EAST text detector...")
-net = cv2.dnn.readNet(args["east"])
+    # load the pre-trained EAST text detector
+    print("[INFO] loading EAST text detector...")
+    net = cv2.dnn.readNet("./backend/preprocessing/textbounding/frozen_east_text_detection.pb")
 
-# construct a blob from the image and then perform a forward pass of the model to obtain the two output layer sets
-blob = cv2.dnn.blobFromImage(image, 1.0, (W, H),
-                             (123.68, 116.78, 103.94), swapRB=True, crop=False)
-start = time.time()
-net.setInput(blob)
-(scores, geometry) = net.forward(layerNames)
-end = time.time()
+    # construct a blob from the image and then perform a forward pass of the model to obtain the two output layer sets
+    blob = cv2.dnn.blobFromImage(image, 1.0, (W, H),
+                                (123.68, 116.78, 103.94), swapRB=True, crop=False)
+    start = time.time()
+    net.setInput(blob)
+    (scores, geometry) = net.forward(layerNames)
+    end = time.time()
 
-# show timing information on text prediction
-print("[INFO] text detection took {:.6f} seconds".format(end - start))
+    # show timing information on text prediction
+    print("[INFO] text detection took {:.6f} seconds".format(end - start))
 
-# grab the number of rows and columns from the scores volume, then initialize our set of bounding box rectangles and corresponding confidence scores
-(numRows, numCols) = scores.shape[2:4]
-rects = []
-confidences = []
+    # grab the number of rows and columns from the scores volume, then initialize our set of bounding box rectangles and corresponding confidence scores
+    (numRows, numCols) = scores.shape[2:4]
+    rects = []
+    confidences = []
 
-# loop over the number of rows
-for y in range(0, numRows):
-    # extract the scores (probabilities), followed by the geometrical data used to derive potential bounding box coordinates that surround text
-    scoresData = scores[0, 0, y]
-    xData0 = geometry[0, 0, y]
-    xData1 = geometry[0, 1, y]
-    xData2 = geometry[0, 2, y]
-    xData3 = geometry[0, 3, y]
-    anglesData = geometry[0, 4, y]
+    # loop over the number of rows
+    for i in range(0, numRows):
+        # extract the scores (probabilities), followed by the geometrical data used to derive potential bounding box coordinates that surround text
+        scoresData = scores[0, 0, i]
+        xData0 = geometry[0, 0, i]
+        xData1 = geometry[0, 1, i]
+        xData2 = geometry[0, 2, i]
+        xData3 = geometry[0, 3, i]
+        anglesData = geometry[0, 4, i]
 
-    # loop over the number of columns
-    for x in range(0, numCols):
-        # if our score does not have sufficient probability, ignore it
-        if scoresData[x] < args["min_confidence"]:
-            continue
+        # loop over the number of columns
+        for x in range(0, numCols):
+            # if our score does not have sufficient probability, ignore it
+            if scoresData[x] < minConfidence:
+                continue
 
-        # compute the offset factor as our resulting feature maps will be 4x smaller than the input image
-        (offsetX, offsetY) = (x * 4.0, y * 4.0)
+            # compute the offset factor as our resulting feature maps will be 4x smaller than the input image
+            (offsetX, offsetY) = (x * 4.0, i * 4.0)
 
-        # extract the rotation angle for the prediction and then compute the sin and cosine
-        angle = anglesData[x]
-        cos = np.cos(angle)
-        sin = np.sin(angle)
+            # extract the rotation angle for the prediction and then compute the sin and cosine
+            angle = anglesData[x]
+            cos = np.cos(angle)
+            sin = np.sin(angle)
 
-        # use the geometry volume to derive the width and height of the bounding box
-        h = xData0[x] + xData2[x]
-        w = xData1[x] + xData3[x]
+            # use the geometry volume to derive the width and height of the bounding box
+            h = xData0[x] + xData2[x]
+            w = xData1[x] + xData3[x]
 
-        # compute both the starting and ending (x, y)-coordinates for the text prediction bounding box
-        endX = int(offsetX + (cos * xData1[x]) + (sin * xData2[x]))
-        endY = int(offsetY - (sin * xData1[x]) + (cos * xData2[x]))
-        startX = int(endX - w)
-        startY = int(endY - h)
+            # compute both the starting and ending (x, y)-coordinates for the text prediction bounding box
+            endX = int(offsetX + (cos * xData1[x]) + (sin * xData2[x]))
+            endY = int(offsetY - (sin * xData1[x]) + (cos * xData2[x]))
+            startX = int(endX - w)
+            startY = int(endY - h)
 
-        # add the bounding box coordinates and probability score to our respective lists
-        rects.append((startX, startY, endX, endY))
-        confidences.append(scoresData[x])
+            # add the bounding box coordinates and probability score to our respective lists
+            rects.append((startX, startY, endX, endY))
+            confidences.append(scoresData[x])
 
-# apply non-maxima suppression to suppress weak, overlapping bounding boxes
-boxes = non_max_suppression(np.array(rects), probs=confidences)
+    # apply non-maxima suppression to suppress weak, overlapping bounding boxes
+    boxes = non_max_suppression(np.array(rects), probs=confidences)
 
-count = 1
-# change box width and height -> positive will add pixels and vice-versa
-box_width_padding = 3
-box_height_padding = 3
+    count = 1
+    # change box width and height -> positive will add pixels and vice-versa
+    box_width_padding = 3
+    box_height_padding = 3
 
-temp_image = orig.copy()
+    temp_image = orig.copy()
 
-# delete output folder
-try:
-    shutil.rmtree('./backend/preprocessing/textbounding/output')
-except Exception as e:
-    do = "nothing"
-
-# create empty output folder
-uncreated = 1
-while (uncreated):
+    # delete output folder
     try:
-        os.mkdir('./backend/preprocessing/textbounding/output')
-        uncreated = 0
+        shutil.rmtree('./backend/preprocessing/textbounding/output')
     except Exception as e:
         do = "nothing"
 
-# define crop object
-class Crop(object):
-    def __init__(self, startX, startY, endX, endY):
-        self.startX = startX
-        self.startY = startY
-        self.endX = endX
-        self.endY = endY
+    # create empty output folder
+    uncreated = 1
+    while (uncreated):
+        try:
+            os.mkdir('./backend/preprocessing/textbounding/output')
+            uncreated = 0
+        except Exception as e:
+            do = "nothing"
 
-    def __eq__(self, other):
-        diff = abs(self.startY - other.startY)
-        if (diff <= 10):
-            return self.startX == other.startX
-        else:
-            False
+    # define crop object
+    class Crop(object):
+        def __init__(self, startX, startY, endX, endY):
+            self.startX = startX
+            self.startY = startY
+            self.endX = endX
+            self.endY = endY
 
-    def __lt__(self, other):
-        diff = abs(self.startY - other.startY)
-        if (diff <= 10):
-            return self.startX < other.startX
-        else:
-            return self.startY < other.startY
+        def __eq__(self, other):
+            diff = abs(self.startY - other.startY)
+            if (diff <= 10):
+                return self.startX == other.startX
+            else:
+                False
 
-croppedList = []
+        def __lt__(self, other):
+            diff = abs(self.startY - other.startY)
+            if (diff <= 10):
+                return self.startX < other.startX
+            else:
+                return self.startY < other.startY
 
-# loop over the bounding boxes
-for (startX, startY, endX, endY) in boxes:
-    # scale the bounding box coordinates based on the respective
-    # ratios
-    startX = int(startX * rW) - box_width_padding
-    startY = int(startY * rH) - box_height_padding
-    endX = int(endX * rW) + box_width_padding
-    endY = int(endY * rH) + box_height_padding
+    croppedList = []
 
-    # draw the bounding box on the image
-    cv2.rectangle(orig, (startX, startY), (endX, endY), (0, 255, 0), 2)
+    # loop over the bounding boxes
+    for (startX, startY, endX, endY) in boxes:
+        # scale the bounding box coordinates based on the respective
+        # ratios
+        startX = int(startX * rW) - box_width_padding
+        startY = int(startY * rH) - box_height_padding
+        endX = int(endX * rW) + box_width_padding
+        endY = int(endY * rH) + box_height_padding
 
-    # append to croppedList to sort the images
-    croppedList.append(Crop(startX, startY, endX, endY))
+        # draw the bounding box on the image
+        cv2.rectangle(orig, (startX, startY), (endX, endY), (0, 255, 0), 2)
 
-croppedList = sorted(croppedList)
+        # append to croppedList to sort the images
+        croppedList.append(Crop(startX, startY, endX, endY))
 
-for img in croppedList:
-    roi = temp_image[img.startY:img.endY, img.startX:img.endX]
-    cv2.imwrite("./backend/preprocessing/textbounding/output/" + str(count) + ".jpg", roi)
-    count = count + 1
+    croppedList = sorted(croppedList)
 
-# show the output image
-cv2.imwrite("./backend/preprocessing/textbounding/output/Text Detection.jpg", orig)
-cv2.waitKey(0)
+    for img in croppedList:
+        roi = temp_image[img.startY:img.endY, img.startX:img.endX]
+        cv2.imwrite("./backend/preprocessing/textbounding/output/" + str(count) + ".jpg", roi)
+        count = count + 1
+
+    # show the output image
+    cv2.imwrite("./backend/preprocessing/textbounding/output/Text Detection.jpg", orig)
+    cv2.waitKey(0)
+    
+    return
