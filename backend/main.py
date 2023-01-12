a3e5c54484d9f63806040d4bc13d4b8a6e46353a
nsamelson <salocin.sam@gmail.com>
2023-01-12 14:43:08 +0100

    add upload json-content in realDB

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 import firebase_admin
 from firebase_admin import storage
 from firebase_admin import credentials
+from firebase_admin import db
 import preprocessing.preprocessing as prep
 import os
 
@@ -10,10 +11,13 @@ import os
 # Firebase setup
 cred = credentials.Certificate('./config/firebase.json')
 firebase_admin.initialize_app(cred, {
-   'storageBucket': 'ocr-des-enfers.appspot.com'
+   'storageBucket': 'ocr-des-enfers.appspot.com',
+   "databaseURL": "https://ocr-des-enfers-default-rtdb.europe-west1.firebasedatabase.app/" 
 })
 
 bucket = storage.bucket()
+ref = db.reference('processed')
+
 
 # Flask setup
 app = Flask(__name__)
@@ -23,8 +27,6 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 # TEST SERVER ONLY
 @app.route('/')
 def hello_world():
-   # blob = bucket.blob("processed/green dragonborn paladin/green dragonborn paladin.jpg")
-   # blob.download_to_filename("backend/temp/"+"green dragonborn paladin.jpg") 
    response = jsonify({'some': 'data'})
    return response
 
@@ -34,13 +36,11 @@ def processData():
        
    # Get url of picture
    items = request.get_json()
-   print("AAAAAAAAAAAAAAAA")
+   print("Got following content : ")
    print(items)
    imageName = items["img"]
-   # imageUrl = items["url"]
+   imageUrl = items["url"]
    folderName = items["folder"]
-
-   # print(imageName, imageUrl)
 
    # Download image from url
    directory = "temp"
@@ -48,7 +48,7 @@ def processData():
       os.makedirs(directory)
    
    blob = bucket.blob("processed/{}/{}".format(folderName,imageName))
-   blob.download_to_filename("/temp/"+imageName) 
+   blob.download_to_filename("temp/"+imageName) 
 
    # TODO: call the preprocessing program 
    prep.prepareCharacters("./backend/temp/"+imageName)
@@ -56,16 +56,23 @@ def processData():
    #    - make bounding boxes in 28*28px
    # TODO: iterative call the processing program (which will loop through every bouding box and send it to model)
    # TODO: call the post processing program which will send back a json
-   # TODO: send the json to firebase/realtime database in a folder with its corresponding image
+
+   # Send the json to firebase/realtime database in a folder with its corresponding image
+   output_ref = ref.child(folderName)
+   output_ref.set({
+      'Name': imageName,
+      'url': imageUrl,
+      'content':{
+         "test" :"1, 2",
+         "other test": "1,2,3,4"
+      }
+   })
    # TODO: send back a success response!
 
    # TODO: remove temp content
    
    response = jsonify({'success': 'The image has been processed!!!'})
    return response
-
-# GET LIST OF UNPROCESSED IMAGES
-
 
 
 if __name__ == '__main__':
